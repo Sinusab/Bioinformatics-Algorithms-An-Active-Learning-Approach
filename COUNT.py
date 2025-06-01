3e0b7a2155db251ee40b093d613e7d6df2a9f879
Sina Abyar <134528508+Sinusab@users.noreply.github.com>
2025-06-01 18:21:22 +0330

    Create COUNT.py

---
+++ COUNT.py
@@ -0,0 +1,9 @@
+def COUNT(Text,Pattern):
+    k = len(Pattern)
+    num = 0
+    for i in range(len(Text)-k+1):
+        if Text[i:i+k] == Pattern:
+            num+=1
+    return num
+
+COUNT('ACAACTATGCATACTATCGGGAACTATCCT','ACTAT')
